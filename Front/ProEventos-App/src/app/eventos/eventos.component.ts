f6fba2242b325bd751a40cfb4dcc32b273ce6073
Luiz Mattos <luizrmattos@gmail.com>
2022-01-22 19:31:16 -0300

    Secao 5 - Angular Interpolacao

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,8 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
 
+
+  public eventosFiltrados: any = [];
+  public eventos: any = [];
+
+  private _filtrolista: string = '';
+
+  public get filtrolista() {
+    return this._filtrolista;
+  }
+
+  public set filtrolista(value: string) {
+    this._filtrolista = value;
+    this.eventosFiltrados = this.filtrolista ? this.filtrarEventos(this.filtrolista) :  this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleUpperCase();
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleUpperCase().indexOf(filtrarPor) !== -1 ||
+      evento.local.toLocaleUpperCase().indexOf(filtrarPor) !== -1
+    )
+  }
   constructor(
     private http: HttpClient
   ) { }
@@ -20,7 +41,10 @@ export class EventosComponent implements OnInit {
 
   public getEventos(): any {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = response;
+      },
       error => console.log(error),
     );
   }
