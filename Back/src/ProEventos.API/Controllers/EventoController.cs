c795f249062ed7f7f62175f8002fdf809172962a
Luiz Mattos <luizrmattos@gmail.com>
2022-01-21 22:07:40 -0300

    Secao 3 - EF

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -12,57 +13,39 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-       public IEnumerable<Evento> _evento = new Evento[] {
-          new Evento() {
-                  EventoId = 1,
-                  Tema = "Angular",
-                  Local = "BH",
-                  Lote = "1 Lote",
-                  QtdPessoas = 250,
-                  DataEvento = DateTime.Now.AddDays(2).ToString(),
-                  ImageURL = "foto1.jpg"
-              },
-              new Evento() {
-                  EventoId = 2,
-                  Tema = "Delphi",
-                  Local = "SP",
-                  Lote = "2 Lote",
-                  QtdPessoas = 120,
-                  DataEvento = DateTime.Now.AddDays(1).ToString(),
-                  ImageURL = "foto2.jpg"
-              }
-       };
-        private readonly ILogger<EventoController> _logger;
+       
+        private readonly DataContext _context;
 
-        public EventoController()
+        public EventoController(DataContext context)
         {
+            _context = context;
 
         }
 
         [HttpPost]
         public string Post()
         {
-           return "value - Post";
+            return "value - Post";
         }
-        
+
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-           return _evento;
+            return _context.Eventos;
         }
 
         [HttpGet("{id}")]
-        public IEnumerable<Evento> GetById(int id)
+        public Evento GetById(int id)
         {
-           return _evento.Where(e => e.EventoId == id);
+            return  _context.Eventos.FirstOrDefault(e => e.EventoId == id);
         }
 
         [HttpDelete("{id}")]
         public string Delete(int id)
         {
-           return $"value = {id}";
+            return $"value = {id}";
         }
-        
-       
+
+
     }
 }
